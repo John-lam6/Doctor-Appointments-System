49a353185565683a72e42469a5f347f7fb1fc247
John-lam6 <johnathanlam0406@gmail.com>
2025-01-07 23:26:04 -0500

    Delete project-deliverable-2-jj directory

--- project-deliverable-2-jj/code/AvailabilityObserver.cpp
+++
@@ -1,8 +0,0 @@
-#include "AvailabilityObserver.h"
-
-#include <iostream>
-
-////TODO Chnage this to the actual update logic
-void AvailabilityObserver::update(const int& doctorid) {
-    std::cout << "Observer notified: Availability of Doctor " << doctorid << " has changed.\n";
-}
